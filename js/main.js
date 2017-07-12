754b204186a7d6449122c16a510ce109c90e8d8d
fellowhacker <naveenmanikanta111@gmail.com>
2017-07-12 21:48:52 +0530

    location and joined date added

--- js/main.js
+++ js/main.js
@@ -80,8 +80,19 @@ function getBasicInfo(data) {
     var imgUrl = data.avatar_url
     var gists = data.public_gists
     var location = data.location
+    var blog = data.blog
+    var joined  = data.created_at
+    var loca = ''
+    var blogdis = ''
+    joined  = "<span style='color:#e44c65'>Joined at </span>" + getmonth(joined.substr(5,6).substr(0,2)) +  " , " + joined.substr(0,4)
     var xx = data.repos_url + '?page=1&per_page=100';
     getrepos(xx)
+    if(location) {
+      loca =  '<span> <img src="img/locat.png" width="27px"> <span style="color:white">' + location + '</span> <br>  </span>'
+    }
+    if(blog) {
+      blogdis = '  <img src="img/blog.png" width="25px"> <span style="color:white"><a href="' + blog + '" target="blank">' + blog + '</a></span> <br>  '
+    }
  
 
     var output =  '<div class="row">' + 
@@ -90,8 +101,12 @@ function getBasicInfo(data) {
 
 			'<div class="col-md-8 text-center">' + 
 				'<img src="' + imgUrl + '" style="width: 150px;height: 150px;margin:0 auto" class="img-responsive img-circle">' + 
-				'<h3 style="color:white"><a href=" ' + url + ' " style="text-decoration: none" target="blank">' + name + '</a> <br><span style="color: #e44c65;">Username:-</span><a href="'+ url + '" style="text-decoration: none" target="blank"> ' + username + '</a><br>' +
-				'<span style="color: #e44c65;">Repositories:-</span><span style="color:white">' + repos + '</span> <br>' + 
+				'<h3 style="color:white"><a href=" ' + url + ' " style="text-decoration: none" target="blank">' + name + '</a> <br><span style="color: #e44c65;">Username:-</span><a href="'+ url + '" style="text-decoration: none" target="blank"> ' + username + '</a><br> '
+         + loca   + blogdis + 
+			//	' <span> <img src="img/locat.png" width="27px"> <span style="color:white">' + location + '</span> <br>  </span> '+loca+' ' + 
+      //  '  <img src="img/blog.png" width="25px"> <span style="color:white"><a href="' + blog + '" target="blank">' + blog + '</a></span> <br>  ' + 
+        '  <img src="img/joined.png" width="27px"> <span style="color:white">' + joined + '</span> <br>  ' +
+        '<span style="color: #e44c65;">Repositories:-</span><span style="color:white">' + repos + '</span> <br>' + 
 				'<span style="color: #e44c65;" class="fol">Followers:- </span>' + followers + ' &nbsp;&nbsp;&nbsp;&nbsp; <span style="color: #e44c65;" class="fol">Following:- </span> ' + following  +
 				' <br><span style="color: #e44c65;">Bio:- </span><span style="color:white"> ' + bio + ' </span> <br> </h3>' +
         '<br><a href="#goToRepos" id="disNone1"><button class="btn btn-success" style="" id="butrep"><span style="font-size: 140%;">Click to view Repositories</span></button></a>' +
@@ -160,4 +175,44 @@ function dispRep(data, i) {
 }
 
 
- 
+function getmonth(month) {
+
+  if(month == 01) {
+    return "Jan"
+  }
+  else if (month == 02){
+    return "Feb"
+  }
+  else if (month == 03){
+    return "Mar"
+  }
+  else if (month == 04){
+    return "April"
+  }
+  else if (month == 05){
+    return "May"
+  }
+  else if (month == 06){
+    return "June"
+  }
+  else if (month == 07){
+    return "July"
+  }
+  else if (month == 08){
+    return "Aug"
+  }
+  else if (month == 09){
+    return "Sep"
+  }
+  else if (month == 10){
+    return "Oct"
+  }
+  else if (month == 11){
+    return "Nov"
+  }
+  else if(month == 12){
+    return "Dec"
+  }
+
+
+}
